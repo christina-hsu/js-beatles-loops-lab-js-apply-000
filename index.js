a2ba88bfd8979c4a09a65882ec71695224a1d55c
Christina HSu <christinahsu.th@gmail.com>
2018-03-07 22:22:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 function theBeatlesPlay(musicians, instruments){
   var arr = [];
   for (var i = 0; i < musicians.length ; i++) { 
-    var str = musicians[i] + " plays " + instruments[0]
-    arr.push.str
+    arr.push(musicians[i] + " plays " + instruments[0]);
 }
 }
